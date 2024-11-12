53c08edd9bc6f633716a54e081f594b43d4b104f
Yassir Rachad <yassirrachad1997@gmail.com>
2024-11-12 17:20:43 +0100

    modification port du connexion

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const {PORT} = process.env;
+  try{
+    await app.listen(PORT,() => console.log(`Running on port ${PORT}`));
+  }catch(err){
+console.log(err);
+  }
+
 }
 bootstrap();
