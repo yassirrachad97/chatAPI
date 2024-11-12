bb2e09cd66ab95ce6945ea5d84b245e87dc32701
Bilal-EZ-ZAIM <bilalzail499@gmail.com>
2024-11-12 14:18:33 +0100

    m

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,7 +6,7 @@ import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { MessagesModule } from './messages/messages.module';
 import { MongooseModule } from '@nestjs/mongoose';
-
+//
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/chat-v-youcode'),
