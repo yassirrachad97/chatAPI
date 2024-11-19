ffaa004f4a94a97359515582af4eb37a515ea49d
Bilal-EZ-ZAIM <bilalzail499@gmail.com>
2024-11-19 12:02:16 +0100

    ajouter effict typing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button"
 import './App.css'
 import Chat from "./pages/Chat"
+import Chatss from "./pages/Chatss"
 
 function App() {
  
 
   return (
     <Chat />
+    // <Chatss/>
   )
 }
 
